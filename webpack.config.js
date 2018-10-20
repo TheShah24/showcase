522d72f91dde8171dc8bfb342b84d5e63defc225
Shahriyar <shahriyarmavlan@gmail.com>
2018-10-19 18:47:38 -0700

    Complete lazy load

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,12 @@ const path = require('path');
 
 module.exports = {
   mode: "production",
-	entry: "./app/assets/scripts/app.js",
+	entry: {
+   App: "./app/assets/scripts/app.js",
+   Vendor: "./app/assets/scripts/Vendor.js"
+  },
 	output: {
-    filename: "bundle.js",
+    filename: "[name].js",
 		path: path.resolve(__dirname,"./app/temp/scripts")
 	},
   module: {
